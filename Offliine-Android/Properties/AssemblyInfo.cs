d42a0d2b40845906179e2896e0c25bf1a51932bf
skettios <brandonjpascual@gmail.com>
2016-07-21 15:39:46 -0700

    Complete rewrite of front and backend to support plugins

--- Offliine-Android/Properties/AssemblyInfo.cs
+++ Offliine-Android/Properties/AssemblyInfo.cs
@@ -1,20 +1,20 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Offliine")]
+[assembly: AssemblyTitle("Offliine_Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Offliine")]
+[assembly: AssemblyProduct("Offliine-Android")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+[assembly: ComVisible(false)]
 
 // Version information for an assembly consists of the following four values:
 //
